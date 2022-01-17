a3c1ddf6116b776ba26291e7cdf5dd2e9ed3b98a
Prajapati omkar <72141037+prajapatiomkar@users.noreply.github.com>
2022-01-17 09:30:03 +0530

    Update this file

--- BinarySearch.java
+++ BinarySearch.java
@@ -43,9 +43,7 @@ public class BinarySearch {
                 }else {
                     start = mid+1;
                 }
-
             }
-
         }return -1;
     }
 
@@ -53,11 +51,9 @@ public class BinarySearch {
     static int Ascending(int[] arr,int target){
         int start = 0;
         int end = arr.length-1;
-
-
+        
         while (start<=end){
             int mid = start + (end - start)/2;
-
             if(target == arr[mid]){
                 return mid;
             }
@@ -68,7 +64,6 @@ public class BinarySearch {
             }
 
         }return -1;
-
     }
 
     //      Descending Order
