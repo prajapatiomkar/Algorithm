7ff07513e763d87098c09e1fae903e543787aa9f
Prajapati omkar <72141037+prajapatiomkar@users.noreply.github.com>
2022-01-25 19:24:14 +0530

    mergeSortInPlace code in java

---
+++ mergeSortInPlace.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class mergeSortInPlace {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+        int m = s + (e - s) / 2;
+        mergeSortInPlace(arr, 0, m);
+        mergeSortInPlace(arr, m, e);
+        mergeInPlace(arr, s, m, e);
+    }
+    static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int i = s;
+        int j = m;
+        int k = 0;
+        int[] mix = new int[e - s];
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+        }
+    }
+}
